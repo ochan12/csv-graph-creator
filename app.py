9f0409116f8f1f910cd064d5ed0f36c40c4e501c
ochan12 <mateochando@gmail.com>
2020-02-04 01:13:05 -0300

    Futura, mejor organizaion de titles, andando OK

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 import matplotlib.pyplot as plt  # Pie chart
 from matplotlib import font_manager as fm
 import matplotlib as mpl
-import csv, random
+import csv, random, os
 import pandas as pd
 import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
+import matplotlib.patches as mpatches
+from matplotlib.legend_handler import HandlerPatch
+from textwrap import wrap
 from constants import MATERIAL_COLORS, get_colors
 print(fm._fmcache)
+FPATH = os.path.join('.', "fonts/futura/Futura Book font.ttf")
+PROP = fm.FontProperties(fname=FPATH)
 
+class HandlerEllipse(HandlerPatch):
+    def create_artists(self, legend, orig_handle,
+                       xdescent, ydescent, width, height, fontsize, trans):
+        center = 0.5 * width - 0.5 * xdescent, 0.5 * height - 0.5 * ydescent
+        p = mpatches.Ellipse(xy=center, width=height + xdescent,
+                             height=height + ydescent)
+        self.update_prop(p, orig_handle, legend)
+        p.set_transform(trans)
+        return [p]
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.files = list()
         self.column_colors = dict()
         self.directory = ""
+        self.title = ""
 
         layout = QtWidgets.QVBoxLayout()
         file_layout = QtWidgets.QHBoxLayout()
         self.setWindowTitle("CSV Chart Creator")
-        self.setFixedHeight(400)
-        self.setFixedWidth(600)
+        self.setMinimumHeight(400)
+        self.setMinimumWidth(600)
 
         self.create_table_widget()
 
@@ -48,6 +63,8 @@ class MainWindow(QtWidgets.QWidget):
 
         file_layout.addWidget(self.colorTable)
 
+        self.create_label_title()
+
         self.create_radio_options()
 
         self.create_label_color()
@@ -62,6 +79,7 @@ class MainWindow(QtWidgets.QWidget):
 
         layout.addLayout(file_layout)
         layout.addLayout(self.radio_options_layout)
+        layout.addLayout(self.title_label_layout)
         layout.addLayout(self.graph_type_layout)
         layout.addLayout(self.color_label_layout)
         layout.addLayout(directory_layout)
@@ -84,6 +102,12 @@ class MainWindow(QtWidgets.QWidget):
         self.color_label_layout.addWidget(QtWidgets.QLabel("Color de texto"))
         self.label_color_field = QtWidgets.QLineEdit()
         self.color_label_layout.addWidget(self.label_color_field)
+
+    def create_label_title(self):
+        self.title_label_layout = QtWidgets.QHBoxLayout()
+        self.title_label_layout.addWidget(QtWidgets.QLabel("Título"))
+        self.label_title_field = QtWidgets.QLineEdit()
+        self.title_label_layout.addWidget(self.label_title_field)
 
     def create_radio_options(self):
         self.radio_options_layout = QtWidgets.QHBoxLayout()
@@ -130,27 +154,40 @@ class MainWindow(QtWidgets.QWidget):
         header = self.tableView.horizontalHeader()
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
 
-    def create_pie_chart(self, sizes, colors, labels, file):
+    def create_pie_chart(self, sizes, colors, file, labels):
+        
         fig1, ax1 = plt.subplots()
-        patches, texts, autotexts = ax1.pie(sizes, colors=colors, labels=labels,
-                                            autopct='%1.1f%%', startangle=90)
-        for text in texts:
-            text.set_color('grey')
-            text.set_family('Bebas')
+        patches, texts, autotexts = ax1.pie(sizes, colors=colors, labels=None,
+                                            autopct='%1.0f%%', pctdistance=1.2, radius=0.4)
+
+        c = [ mpatches.Circle((0.5, 0.5), radius = 0.25, facecolor=colors[i], edgecolor="none" ) for i in range(len(texts))]
+        plt.legend(c,labels,handler_map={mpatches.Circle: HandlerEllipse()}, loc='center left', bbox_to_anchor=(1.1,0.5), frameon=False, prop=PROP)
+        title = file.replace('.csv', '').replace('_groupped', '').replace('_', ' ').upper()
+        
+        fig1.suptitle("\n".join(wrap(title, 60)), fontproperties=PROP)
+        
+        #for text in texts:
+         #   text.set_color('white')
+         #   text.set_font_properties(PROP)
         for autotext in autotexts:
             autotext.set_color('grey')
+            autotext.set_font_properties(PROP)
+
         ax1.axis('equal')
         plt.tight_layout()
         plt.savefig(self.directory+'/'+file.replace('.csv', '')+'_pie.eps')
 
     def create_bar_chart(self, sizes, colors, labels, file):
         print("Creating bar chart")
+        plt.legend(loc='center', borderpad=0.0, fontproperties=PROP)
         fig, ax = plt.subplots()
         index = np.arange(len(labels))
         bar_width = 0.35
         opacity = 0.8
-        plt.bar(index, sizes, bar_width, alpha=opacity, color=colors)
+        plt.bar(index, sizes, bar_width, alpha=opacity, color=colors, fontproperties=PROP)
+        plt.title(file.replace('.csv', '').upper())
         plt.xticks(index, labels)
+        plt.legend()
         plt.tight_layout()
         plt.savefig(self.directory+'/'+file.replace('.csv', '')+'_bar.eps')
         print("Finished bar chart")
@@ -162,23 +199,33 @@ class MainWindow(QtWidgets.QWidget):
                 open_file = csv.DictReader(open(file[1]))
                 custom_color = [self.column_colors[key]
                                 for key in open_file.fieldnames]
+                print("fields")
+                print(open_file.fieldnames)
                 if self.file_per_row_btn.isChecked():
                     i = 1
                     for row in open_file:
-                        sizes = [v for k, v in row.items()]
+                        sizes = [v for k, v in row.items() if int(v) > 0]
                         if self.graph_type_combo.currentIndex() == 0:
                             self.create_pie_chart(
-                                sizes=sizes, colors=custom_color, labels=open_file.fieldnames, file=file[0]+'_'+str(i))
+                                sizes=sizes, 
+                                colors=custom_color, 
+                                labels=open_file.fieldnames, 
+                                file=file[0])
+
                         elif self.graph_type_combo.currentIndex() == 1:
                             self.create_bar_chart(
-                                sizes=sizes, colors=custom_color, labels=open_file.fieldnames, file=file[0]+'_'+str(i))
+                                sizes=sizes, colors=custom_color, labels=open_file.fieldnames, file=file[0])
                         i += 1
                 else:
                     df = pd.read_csv(file[1])
                     sizes = [df[column].sum() for column in df.columns]
                     if self.graph_type_combo.currentIndex() == 0:
                         self.create_pie_chart(
-                            sizes=sizes, colors=custom_color, labels=open_file.fieldnames, file=file[0]+'_groupped')
+                            sizes=sizes, 
+                            colors=custom_color, 
+                            labels=open_file.fieldnames, 
+                            file=file[0]+'_groupped')
+
                     elif self.graph_type_combo.currentIndex() == 1:
                         self.create_bar_chart(
                             sizes=sizes, colors=custom_color, labels=open_file.fieldnames, file=file[0]+'_groupped')
